6a60013fcc0fccb947bdd8cee2c05115d1c7f54f
yncnkaya <ync_kaya@hotmail.com>
2024-07-12 22:52:37 +0300

    31. ödev eklendi.

---
+++ Odev-31.js
@@ -0,0 +1,9 @@
+/* 
+1. Bir dizideki kelimelerin sadece "e" harfi ile bitenlerini filtrelemek için filter metodunu kullanın.
+
+Örnek Dizi: ['apple', 'banana', 'cherry', 'date', 'grape']
+
+2. Bir dizideki her kelimenin uzunluğunu döndüren bir map fonksiyonu yazın.
+
+Örnek Dizi: ['lion', 'tiger', 'elephant', 'cat']
+*/
