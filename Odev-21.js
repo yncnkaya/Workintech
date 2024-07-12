6f3c7cd691eba70d00c2aad1712a823a58af5b4d
Nilufer Ulgen <nilufer.korkmaz@gmail.com>
2024-07-12 22:21:02 +0300

    Odev_21 Cozumu

--- Odev-21.js
+++ Odev-21.js
@@ -7,3 +7,14 @@
 
 Örnek Dizi: [15, 3, 9, 6, 2, 20, 7]
 */
+
+// Ödev -1
+const meyveler = ['orange', 'apple', 'banana', 'kiwi', 'grape'];
+meyveler.sort();
+console.log(meyveler);
+
+// Ödev -2
+const sayilar = [ 15, 3, 9, 6, 2, 20, 7 ];
+sayilar.sort(function(a,b){return b-a});
+
+console.log(sayilar);
